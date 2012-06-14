5782666e46f334fedeefc9a4af46573f4be9e004
Niu Yi <niuyi2010@gmail.com>
2012-06-14 10:57:46 +0800

    add icon list

--- src/com/niuyi/test/PinDaoActivity.java
+++ src/com/niuyi/test/PinDaoActivity.java
@@ -1,22 +1,114 @@
 package com.niuyi.test;
 
+
 import android.app.Activity;
-import android.app.ListActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class PinDaoActivity extends Activity {
+	
+	private class EfficientAdapter extends BaseAdapter {
+        private LayoutInflater flater;
+        private Bitmap icon;
+
+        public EfficientAdapter(Context context) {
+            // Cache the LayoutInflate to avoid asking for a new one each time.
+            flater = LayoutInflater.from(context);
+
+            // Icons bound to the rows.
+            icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.label_icon);
+        }
+
+        /**
+         * The number of items in the list is determined by the number of speeches
+         * in our array.
+         *
+         * @see android.widget.ListAdapter#getCount()
+         */
+        public int getCount() {
+            return names.length;
+        }
+
+        /**
+         * Since the data comes from an array, just returning the index is
+         * sufficent to get at the data. If we were using a more complex data
+         * structure, we would return whatever object represents one row in the
+         * list.
+         *
+         * @see android.widget.ListAdapter#getItem(int)
+         */
+        public Object getItem(int position) {
+            return position;
+        }
+
+        /**
+         * Use the array index as a unique id.
+         *
+         * @see android.widget.ListAdapter#getItemId(int)
+         */
+        public long getItemId(int position) {
+            return position;
+        }
+
+        /**
+         * Make a view to hold each row.
+         *
+         * @see android.widget.ListAdapter#getView(int, android.view.View,
+         *      android.view.ViewGroup)
+         */
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // A ViewHolder keeps references to children views to avoid unneccessary calls
+            // to findViewById() on each row.
+            ViewHolder holder;
+
+            // When convertView is not null, we can reuse it directly, there is no need
+            // to reinflate it. We only inflate a new View when the convertView supplied
+            // by ListView is null.
+            if (convertView == null) {
+                convertView = flater.inflate(R.layout.list_item, null);
+
+                // Creates a ViewHolder and store references to the two children views
+                // we want to bind data to.
+                holder = new ViewHolder();
+                holder.text = (TextView) convertView.findViewById(R.id.text);
+                holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+
+                convertView.setTag(holder);
+            } else {
+                // Get the ViewHolder back to get fast access to the TextView
+                // and the ImageView.
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            // Bind the data efficiently with the holder.
+            holder.text.setText(names[position]);
+            holder.icon.setImageBitmap(icon);
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView text;
+            ImageView icon;
+        }
+    }
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pindao);
 		ListView listView =  (ListView)this.findViewById(R.id.listView1);
-		listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, names));
-		listView.setTextFilterEnabled(true);
-		listView.setHeaderDividersEnabled(true);
+		listView.setAdapter(new EfficientAdapter(this));
 	}
 	
 	private final String[] names = new String[]{
