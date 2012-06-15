675196064e00f44c290c2f1bd9a395d6788461d4
Niu Yi <niuyi2010@gmail.com>
2012-06-15 19:09:37 +0800

    add read

--- src/com/niuyi/test/SearchActivity.java
+++ src/com/niuyi/test/SearchActivity.java
@@ -1,5 +1,15 @@
 package com.niuyi.test;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +19,7 @@ import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -21,65 +32,65 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class SearchActivity extends Activity {
-	
+
 	static final String[] COUNTRIES = new String[] { "Afghanistan", "Albania",
-		"Algeria", "American Samoa", "Andorra", "Angola", "Anguilla",
-		"Antarctica", "Antigua and Barbuda", "Argentina", "Armenia",
-		"Aruba", "Australia", "Austria", "Azerbaijan", "Bahrain",
-		"Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
-		"Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina",
-		"Botswana", "Bouvet Island", "Brazil",
-		"British Indian Ocean Territory", "British Virgin Islands",
-		"Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cote d'Ivoire",
-		"Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
-		"Central African Republic", "Chad", "Chile", "China",
-		"Christmas Island", "Cocos (Keeling) Islands", "Colombia",
-		"Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia",
-		"Cuba", "Cyprus", "Czech Republic",
-		"Democratic Republic of the Congo", "Denmark", "Djibouti",
-		"Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt",
-		"El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
-		"Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji",
-		"Finland", "Former Yugoslav Republic of Macedonia", "France",
-		"French Guiana", "French Polynesia", "French Southern Territories",
-		"Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
-		"Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala",
-		"Guinea", "Guinea-Bissau", "Guyana", "Haiti",
-		"Heard Island and McDonald Islands", "Honduras", "Hong Kong",
-		"Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq",
-		"Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
-		"Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-		"Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
-		"Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar",
-		"Malawi", "Malaysia", "Maldives", "Mali", "Malta",
-		"Marshall Islands", "Martinique", "Mauritania", "Mauritius",
-		"Mayotte", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia",
-		"Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-		"Nauru", "Nepal", "Netherlands", "Netherlands Antilles",
-		"New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria",
-		"Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-		"Norway", "Oman", "Pakistan", "Palau", "Panama",
-		"Papua New Guinea", "Paraguay", "Peru", "Philippines",
-		"Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-		"Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe",
-		"Saint Helena", "Saint Kitts and Nevis", "Saint Lucia",
-		"Saint Pierre and Miquelon", "Saint Vincent and the Grenadines",
-		"Samoa", "San Marino", "Saudi Arabia", "Senegal", "Seychelles",
-		"Sierra Leone", "Singapore", "Slovakia", "Slovenia",
-		"Solomon Islands", "Somalia", "South Africa",
-		"South Georgia and the South Sandwich Islands", "South Korea",
-		"Spain", "Sri Lanka", "Sudan", "Suriname",
-		"Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland",
-		"Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand",
-		"The Bahamas", "The Gambia", "Togo", "Tokelau", "Tonga",
-		"Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
-		"Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-		"Ukraine", "United Arab Emirates", "United Kingdom",
-		"United States", "United States Minor Outlying Islands", "Uruguay",
-		"Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
-		"Wallis and Futuna", "Western Sahara", "Yemen", "Yugoslavia",
-		"Zambia", "Zimbabwe" };
-	
+			"Algeria", "American Samoa", "Andorra", "Angola", "Anguilla",
+			"Antarctica", "Antigua and Barbuda", "Argentina", "Armenia",
+			"Aruba", "Australia", "Austria", "Azerbaijan", "Bahrain",
+			"Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
+			"Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina",
+			"Botswana", "Bouvet Island", "Brazil",
+			"British Indian Ocean Territory", "British Virgin Islands",
+			"Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cote d'Ivoire",
+			"Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
+			"Central African Republic", "Chad", "Chile", "China",
+			"Christmas Island", "Cocos (Keeling) Islands", "Colombia",
+			"Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia",
+			"Cuba", "Cyprus", "Czech Republic",
+			"Democratic Republic of the Congo", "Denmark", "Djibouti",
+			"Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt",
+			"El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
+			"Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji",
+			"Finland", "Former Yugoslav Republic of Macedonia", "France",
+			"French Guiana", "French Polynesia", "French Southern Territories",
+			"Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
+			"Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala",
+			"Guinea", "Guinea-Bissau", "Guyana", "Haiti",
+			"Heard Island and McDonald Islands", "Honduras", "Hong Kong",
+			"Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq",
+			"Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+			"Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
+			"Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+			"Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar",
+			"Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+			"Marshall Islands", "Martinique", "Mauritania", "Mauritius",
+			"Mayotte", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia",
+			"Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
+			"Nauru", "Nepal", "Netherlands", "Netherlands Antilles",
+			"New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria",
+			"Niue", "Norfolk Island", "North Korea", "Northern Marianas",
+			"Norway", "Oman", "Pakistan", "Palau", "Panama",
+			"Papua New Guinea", "Paraguay", "Peru", "Philippines",
+			"Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
+			"Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe",
+			"Saint Helena", "Saint Kitts and Nevis", "Saint Lucia",
+			"Saint Pierre and Miquelon", "Saint Vincent and the Grenadines",
+			"Samoa", "San Marino", "Saudi Arabia", "Senegal", "Seychelles",
+			"Sierra Leone", "Singapore", "Slovakia", "Slovenia",
+			"Solomon Islands", "Somalia", "South Africa",
+			"South Georgia and the South Sandwich Islands", "South Korea",
+			"Spain", "Sri Lanka", "Sudan", "Suriname",
+			"Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland",
+			"Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand",
+			"The Bahamas", "The Gambia", "Togo", "Tokelau", "Tonga",
+			"Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
+			"Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
+			"Ukraine", "United Arab Emirates", "United Kingdom",
+			"United States", "United States Minor Outlying Islands", "Uruguay",
+			"Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
+			"Wallis and Futuna", "Western Sahara", "Yemen", "Yugoslavia",
+			"Zambia", "Zimbabwe" };
+
 	private AutoCompleteTextView textView;
 
 	private SearchResultListAdapter searchResultListAdapter;
@@ -88,35 +99,88 @@ public class SearchActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.search);
-		textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line, COUNTRIES);
-		textView.setAdapter(adapter);
-		
-		ListView searchResultListView = (ListView)findViewById(R.id.search_result_list);
-		searchResultListView.setEmptyView(this.findViewById(R.id.empty));
-		searchResultListAdapter = new SearchResultListAdapter(this);
-		searchResultListView.setAdapter(searchResultListAdapter);
-		
-		Button searchButton = (Button)this.findViewById(R.id.search_button); 
-		searchButton.setOnClickListener(new OnClickListener(){
+		setupSearchTextView();
+		setupSearchButton();
+		setupSearchResultList();
+	}
+
+	private void setupSearchButton() {
+		Button searchButton = (Button) this.findViewById(R.id.search_button);
+		searchButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				String text = textView.getText().toString();
 				searchResultListAdapter.addResult(text);
+				writeToFile(text);
 			}
-			
+
+			private void writeToFile(String text) {
+				BufferedWriter bufferedWriter = null;
+				try {
+					bufferedWriter = new BufferedWriter(new OutputStreamWriter(
+							openFileOutput("search_history.txt",
+									Context.MODE_APPEND), "UTF-8"));
+					bufferedWriter.write(text);
+					bufferedWriter.newLine();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if (bufferedWriter != null) {
+						try {
+							bufferedWriter.close();
+						} catch (IOException e) {
+						}
+					}
+				}
+			}
 		});
 	}
-	
-	class SearchResultListAdapter extends BaseAdapter{
-		
+
+	private void setupSearchResultList() {
+		ListView searchResultListView = (ListView) findViewById(R.id.search_result_list);
+		searchResultListView.setEmptyView(this.findViewById(R.id.empty));
+		searchResultListAdapter = new SearchResultListAdapter(this);
+		readFromFile();
+		searchResultListView.setAdapter(searchResultListAdapter);
+	}
+
+	private void readFromFile() {
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(
+												new InputStreamReader(
+														openFileInput("search_history.txt")));
+			String line;
+			while((line = bufferedReader.readLine()) != null){
+				searchResultListAdapter.addResult(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
+	private void setupSearchTextView() {
+		setContentView(R.layout.search);
+		textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+		textView.setAdapter(adapter);
+	}
+
+	class SearchResultListAdapter extends BaseAdapter {
+
 		private List<String> results = new ArrayList<String>();
 		private final Context context;
 
-		public SearchResultListAdapter(Context context){
+		public SearchResultListAdapter(Context context) {
 			this.context = context;
 		}
 
@@ -144,11 +208,11 @@ public class SearchActivity extends Activity {
 			textView.setTextSize(20);
 			return textView;
 		}
-		
-		public void addResult(String result){
+
+		public void addResult(String result) {
 			results.add(result);
 			notifyDataSetChanged();
 		}
-		
+
 	}
 }
