b633b8891ae8c71bb3039e77d012539150024338
Niu Yi <niuyi2010@gmail.com>
2012-06-15 10:43:47 +0800

    add popup

--- src/com/niuyi/test/MovieActivity.java
+++ src/com/niuyi/test/MovieActivity.java
@@ -2,12 +2,18 @@ package com.niuyi.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MovieActivity extends Activity {
+	private PopupWindow popupWindow;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -17,6 +23,35 @@ public class MovieActivity extends Activity {
 		
 		Button button = (Button)findViewById(R.id.button1);
 		button.setBackgroundDrawable(this.getResources().getDrawable(R.drawable.corners_button));
+		
+		button.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(popupWindow == null){
+					View popup = MovieActivity.this.getLayoutInflater().inflate(R.layout.pop, null, true);
+					popupWindow = new PopupWindow(popup, 100, 170, true);
+					
+					popup.setOnTouchListener(new OnTouchListener(){
+
+						@Override
+						public boolean onTouch(View v, MotionEvent event) {
+							if(popupWindow != null && popupWindow.isShowing()){
+								popupWindow.dismiss();
+								popupWindow = null;
+								return true;
+							}
+							return false;
+						}
+						
+					});
+				}
+				
+				
+				popupWindow.showAsDropDown(v);
+			}
+			
+		});
 	}
 
 }
--- src/com/niuyi/test/PinDaoActivity.java
+++ src/com/niuyi/test/PinDaoActivity.java
@@ -59,8 +59,6 @@ public class PinDaoActivity extends Activity {
 
             return convertView;
         }
-
-        
     }
 	
 	@Override
